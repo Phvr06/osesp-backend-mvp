e5bf36647b4216ea66f030233fdb844f3f5358d9
Rkzzin <henriquels5@al.insper.edu.br>
2026-05-13 09:00:13 -0300

    add template routes

--- routers/template.py
+++ routers/template.py
@@ -30,3 +30,18 @@ def read_template(template_id: int, db: Session = Depends(get_db)):
     if not template:
         raise HTTPException(status_code=404, detail="Template não encontrado")
     return template
+
+@router.get("/concert/{concert_id}", response_model=List[schemas.template.TemplateResponse])
+def read_templates_by_concert(concert_id: int, db: Session = Depends(get_db)):
+    return db.query(models.TemplateDB).filter(models.TemplateDB.concerto_id == concert_id).all()
+
+@router.put("/{template_id}", response_model=schemas.template.TemplateResponse)
+def update_template(template_id: int, template_update: schemas.template.TemplateCreate, db: Session = Depends(get_db)):
+    db_template = db.query(models.TemplateDB).filter(models.TemplateDB.id == template_id).first()
+    if not db_template:
+        raise HTTPException(status_code=404, detail="Template não encontrado")
+    
+    db_template.conteudo = template_update.conteudo
+    db.commit()
+    db.refresh(db_template)
+    return db_template
