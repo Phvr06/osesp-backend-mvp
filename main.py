ec89e7cc05120d180d6ae9db2f0c3534f025114a
Rkzzin <henriquels5@al.insper.edu.br>
2026-05-13 08:50:36 -0300

    add: CORS

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from database import engine, Base
 import models
+from fastapi.middleware.cors import CORSMiddleware
 from routers import concert, user, message, telegram, template
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SHOW UP - API OSESP", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(concert.router)
 app.include_router(user.router)
